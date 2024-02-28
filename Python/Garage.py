7f6ad785ed1987b15dbc6f991c258be047e83005
AlfiFaiz <134774589+AlfiFaiz@users.noreply.github.com>
2024-02-28 20:24:28 +0700

    Update Garage.py nambahin janji

--- Python/Garage.py
+++ Python/Garage.py
@@ -1,3 +1,7 @@
+#Saya Muhammad Alfi faiz NIM 2207045 mengerjakan
+#soal Latihan 2 dalam mata kuliah Desain Pemograman Berorientasi Objek
+#untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
+
 # Import class Vehicle dan turunannya
 from Vehicle import Vehicle
 from Motorcycle import Motorcycle
