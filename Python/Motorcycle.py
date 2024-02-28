5f5bd1f67a53c72d249ba201b646cd284aa7fd01
AlfiFaiz <134774589+AlfiFaiz@users.noreply.github.com>
2024-02-28 20:25:29 +0700

    Update Motorcycle.py nambanin janji

--- Python/Motorcycle.py
+++ Python/Motorcycle.py
@@ -1,3 +1,7 @@
+#Saya Muhammad Alfi faiz NIM 2207045 mengerjakan
+#soal Latihan 2 dalam mata kuliah Desain Pemograman Berorientasi Objek
+#untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
+
 # Import class Vehicle yang menjadi parent class
 from Vehicle import Vehicle
 
